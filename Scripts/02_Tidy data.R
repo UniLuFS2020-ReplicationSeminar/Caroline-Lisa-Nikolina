98e4534007772d6366e54bf7ac36ba6214e78b38
nikmalb <malbasic.nikolina@gmail.com>
2024-03-25 15:31:33 +0100

    Rename script as "Tidy data"

--- Scripts/02_Tidy data.R
+++ Scripts/02_Tidy data.R
@@ -1,13 +1,15 @@
 #Remove NAs
-
-class(X1000_companies_list_and_rating)
-
-X1000_companies_list_and_rating$age <- as.numeric(gsub("[^0-9]", "", X1000_companies_list_and_rating$age))
-
-
 Company_ratings <- X1000_companies_list_and_rating[!is.na(X1000_companies_list_and_rating$age) & !is.na(X1000_companies_list_and_rating$rating), ]
 
+#Convert the row "age" from character to numeric
+Company_ratings$age <- as.numeric(gsub("[^0-9]", "", Company_ratings$age))
+
+#Remove false ages (2024)
+Company_ratings_tidy <- Company_ratings[Company_ratings$age != 2024, ]
 
 
-View(Company_ratings)
+
+
+#View the tidyed data
+View(Company_ratings_tidy)
 
