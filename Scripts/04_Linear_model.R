126741b053adb8bf9e2b6259f4922e7f1ace1503
CarolineEicher <caroline.eicher@gmx.ch>
2024-03-25 16:50:45 +0100

    linear model

---
+++ Scripts/04_Linear_model.R
@@ -0,0 +1,11 @@
+# Linear model--------------------------------------------
+## Linear model for companies until 400 years--------------------------------------------
+lm400 <- lm(Company_ratings_tidy$rating ~ Company_ratings_tidy$age)
+summary(lm400)
+
+## Linear model for companies until 100 years--------------------------------------------
+Company_ratings_tidy|>
+  filter(age < 100) |>
+  lm(rating ~ age) |>
+  summary()
+
