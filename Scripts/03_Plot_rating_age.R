05a2391c04e3ad73f075d0689e40546ef5b48d39
CarolineEicher <caroline.eicher@gmx.ch>
2024-03-25 15:49:26 +0100

    plot only companies < 100 years

--- Scripts/03_Plot_rating_age.R
+++ Scripts/03_Plot_rating_age.R
@@ -3,7 +3,16 @@ library("tidyverse")
 library("ggplot2")
 
 View(Company_ratings_tidy)
+
 ggplot(Company_ratings_tidy, aes(x = age, y = rating)) +
   geom_point() +
   geom_smooth() +
   labs(x = "Age", y = "Rating", title = "Scatter Plot of company age vs. rating")
+
+# Plot with only companies that are younger than 100 years
+Company_ratings_tidy |> 
+  filter(age < 100) |>
+  ggplot(aes(x = age, y = rating)) +
+  geom_point() +
+  geom_smooth() +
+  labs(x = "Age", y = "Rating", title = "Scatter Plot of company age vs. rating (age < 100)")
