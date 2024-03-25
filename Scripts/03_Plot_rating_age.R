48394de3f7d1e432a7f385ad4d8b86dcd486de41
CarolineEicher <caroline.eicher@gmx.ch>
2024-03-25 14:58:16 +0100

    plot rating and age of the companies

--- Scripts/03_Plot_rating_age.R
+++ Scripts/03_Plot_rating_age.R
@@ -2,6 +2,6 @@ library("dplyr")
 library("tidyverse")
 library("ggplot2")
 
-ggplot(X1000_companies_list_and_rating, aes(x = rating, y = age)) +
+ggplot(Company_ratings, aes(x = rating, y = age)) +
   geom_point() +
   labs(x = "Rating", y = "Age", title = "Scatter Plot of company age vs. rating")
