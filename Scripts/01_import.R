352aa5fdfae180b73787b830ff740819af619f25
CarolineEicher <caroline.eicher@gmx.ch>
2024-03-25 14:29:00 +0100

    Import the dataset

--- Scripts/01_import.R
+++ Scripts/01_import.R
@@ -1,4 +1,4 @@
 #Load data
+library(readr)
+X1000_companies_list_and_rating <- read_csv("Data/1000 companies list and rating.csv")
 View(X1000_companies_list_and_rating)
-
-#this is another test line
