dff3e0034915bb7069e57a1c223da735e23f8410
nikmalb <malbasic.nikolina@gmail.com>
2024-03-25 15:10:18 +0100

    Convert the column "age"

--- Scripts/02_Tidy data.R
+++ Scripts/02_Tidy data.R
@@ -2,5 +2,12 @@
 
 class(X1000_companies_list_and_rating)
 
+X1000_companies_list_and_rating$age <- as.numeric(gsub("[^0-9]", "", X1000_companies_list_and_rating$age))
+
+
 Company_ratings <- X1000_companies_list_and_rating[!is.na(X1000_companies_list_and_rating$age) & !is.na(X1000_companies_list_and_rating$rating), ]
-View(Copmany_ratings)
+
+
+
+View(Company_ratings)
+
