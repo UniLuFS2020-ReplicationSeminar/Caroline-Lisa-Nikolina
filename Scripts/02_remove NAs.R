10b217cdd691558975f3e95d12d72f80d99e8fa7
CarolineEicher <caroline.eicher@gmx.ch>
2024-03-25 14:49:06 +0100

    fixed error in object name

--- Scripts/02_remove NAs.R
+++ Scripts/02_remove NAs.R
@@ -2,5 +2,5 @@
 
 class(X1000_companies_list_and_rating)
 
-Copmany_ratings <- X1000_companies_list_and_rating[!is.na(X1000_companies_list_and_rating$age) & !is.na(X1000_companies_list_and_rating$rating), ]
+Company_ratings <- X1000_companies_list_and_rating[!is.na(X1000_companies_list_and_rating$age) & !is.na(X1000_companies_list_and_rating$rating), ]
 View(Copmany_ratings)
